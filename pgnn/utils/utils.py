4552bf97a88dd5fe21bb97971c6b4df5965b62c9
Antonio Oroz <antonio.oroz@tum.de>
2022-11-12 13:45:28 +0100

    env

--- pgnn/utils/utils.py
+++ pgnn/utils/utils.py
@@ -106,8 +106,8 @@ def config_from_dict(dictionary):
 def get_device():
     if torch.cuda.is_available():
         return torch.device('cuda')
-    elif torch.backends.mps.is_available():
-        return torch.device('mps')
+    #elif torch.backends.mps.is_available():
+    #    return torch.device('mps')
     else:
         return torch.device('cpu')
 
